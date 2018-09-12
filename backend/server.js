126895260bd8e41d31e81acf99e00c6bc24747ba
Unknown <opticalfalicy@gmail.com>
2018-09-12 16:40:24 -0500

    Work on server.js

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,29 @@ const corsOptions = {
   preflightContinue: false,
   optionsSuccessStatus: 204
 };
+
+
+// ================ ROUTERS requires ================ //
+
+// Project
+
+const createProjectRouter = require('./Router/Project/createProjectRouter.js');
+const findProjectRouter = require('./Router/Project/findProjectRouter.js');
+const removeProjectRouter = require('./Router/Project/removeProjectRouter.js');
+const updateProjectRouter = require('./Router/Project/updateProjectRouter.js');
+
+const server = express();
+
+mongoose.connect('mongodb://username:abcd1234@ds161391.mlab.com:61391/lambda-labs', { useNewUrlParser: true })
+
+.then(() => console.log('\n===connected to mongo===\n'))
+.catch(err =>console.log('not connected'))
+
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors(corsOptions));
+
+server.listen(port, () => console.log('API on port 5000'));
