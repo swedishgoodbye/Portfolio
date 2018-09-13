f05a2346dea726db0382091915b4dedc93d06653
Unknown <opticalfalicy@gmail.com>
2018-09-12 19:13:22 -0500

    Branch init, started actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,1 +1,32 @@
 import axios from "axios";
+
+export const FINDINGPOSTS = "FINDINGPOSTS";
+export const FOUNDPOSTS = "FOUNDPOSTS";
+export const CREATINGPOST = "CREATINGPOST";
+export const CREATEDPOST = "CREATEDPOST";
+export const UPDATINGPOST = "UPDATINGPOST";
+export const UPDATEDPOST = "UPDATEDPOST";
+export const DELETINGPOST = "DELETINGPOST";
+export const DELETEDPOST = "DELETEDPOST";
+
+// const URL = "https://spbend.herokuapp.com/api/";
+const URL = "http://localhost:5000/api";
+
+export const getPosts = () => dispatch => {
+
+    dispatch({
+        type: FINDINGPOSTS
+    });
+
+    axios
+        .get(`${URL}/classes`)
+        .then(response => {
+            console.log("FINDING POSTS", response.data.length);
+            const all_posts = response.data;
+            dispatch({ type: FOUNDPOSTS, posts: response.data 
+            })
+        })
+        .catch(err => {
+            dispatch({ type: ERROR, errorMessage: 'Error Fetching Posts!'})
+        });
+}
