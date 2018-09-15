0597b005ae3bfa55c26af5f68e5c7591caa63b09
Unknown <opticalfalicy@gmail.com>
2018-09-15 09:02:53 -0500

    Portfolio component work

--- frontend/src/components/portfolio/index.js
+++ frontend/src/components/portfolio/index.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 
 import { getProjects } from '../../actions';
 import { connect } from 'react-redux';
+import { Modal } from 'reactstrap';
 
 import './portfolio.css';
 
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)',
+      width                 : '90%',
+      height                : '90%'
+    }
+  };
+  
 
 
 class Portfolio extends Component{
@@ -14,33 +28,47 @@ class Portfolio extends Component{
         super(props);
         this.state = {
             // projects: []
+            modal: false
         };
         this.props.getProjects();
+        this.toggle = this.toggle.bind(this);
         // this.state={
         //     project_name: "test",
         //     project_thumb: URL,
         // }
     }
 
-    componentDidMount(){
-        // console.log(this.state);
 
-        let req = require.context('../../media/', false, /.*\.jpg$/)
-        req.keys().forEach(function(key){
-            req(key);
-          });
-          console.log('mount', this);
-    }
-
+    toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
 
     render(){
         console.log('port state', this)
         // const data = this.props.data;
         return(
-            <div className="portfolio-page"> 
+            <div className="portfolio-page">     
+            <div onClick={this.openModal} className="project-card-outer">
+            <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                // className = 'portfolio-modal'
+                contentLabel="Example Modal"
+            >
+
+                      <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                        <button onClick={this.closeModal}>close</button>
+                         <div>I am a modal</div>
+            </Modal>
+
             {this.props.projects.map(projItem => {
                 return (
                 <div className="project-card">
+                
                     <div className="project-name">
                         {projItem.title} 
                     </div>
@@ -53,6 +81,7 @@ class Portfolio extends Component{
                 </div>
             );
             })}
+            </div>
             </div>
         )
     }
