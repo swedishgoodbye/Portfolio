e0da915b547a6b2d9d38e17a13b9c66927ad48e9
Unknown <opticalfalicy@gmail.com>
2018-09-17 17:57:35 -0500

    Opening a card's modal does not select the right project

--- frontend/src/components/portfolio/index.js
+++ frontend/src/components/portfolio/index.js
@@ -13,7 +13,8 @@ const customStyles = {
       right                 : 'auto',
       bottom                : 'auto',
       marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      transform             : 'translate(-50%, -50%)',
+      width                 : '50%'
     }
   };
 
@@ -34,9 +35,21 @@ class Portfolio extends Component{
         // }
     }
 
-    openModal() {
-        this.setState({modalIsOpen: true});
-      }
+    openModal = e => {
+        // console.log(e.target.value)
+        // console.log(this.state)
+        // if(e.target.value == this.props.projects._id){
+        // this.setState({modalIsOpen: true});
+
+        for(let i = 0; i < this.props.projects.length; i++){
+            if(e.target.value == this.props.projects[i]._id){
+                this.setState({modalIsOpen: true})
+            }
+            else{
+                this.setState({modalIsOpen: false})
+            }
+        }
+    }
 
       afterOpenModal() {
         // references are now sync'd and can be accessed.
@@ -74,7 +87,7 @@ class Portfolio extends Component{
                                  <img src={projItem.image}/> 
                                  {/* <img src='https://photos.google.com/u/1/photo/AF1QipOWPVvBb0b7gIbuD4_xhgvBQ63rCWt4OZ1ibQuU'/> */}
                               </div> 
-                             <button onClick={this.openModal}>{projItem.title}</button> 
+                             <button value = {projItem._id} onClick={this.openModal}>{projItem.title}</button> 
                                      <Modal
                                          isOpen={this.state.modalIsOpen}
                                          onAfterOpen={this.afterOpenModal}
@@ -82,16 +95,17 @@ class Portfolio extends Component{
                                          style={customStyles}
                                          contentLabel="Example Modal"
                                          >
-                                         <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                                         <div className="modal-top">
+                                         <img className='project-modal' src={projItem.image}/> 
+                                         <h2 ref={subtitle => this.subtitle = subtitle}>
+                                            {projItem.title} <br/> {projItem.link}
+                                        </h2>
+                                         
+                                         </div>
+                                         <div className="modal-bottom">
+                                         <div>{projItem.description}</div>
                                          <button onClick={this.closeModal}>close</button>
-                                         <div>I am a modal</div>
-                                         <form>
-                                             <input />
-                                             <button>tab navigation</button>
-                                             <button>stays</button>
-                                             <button>inside</button>
-                                             <button>the modal</button>
-                                         </form>
+                                         </div>
                                      </Modal>                                     
                              </div> 
                          </div>      
