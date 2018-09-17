18cbef748e7d851e8125db2593f8efab5f02bf52
Unknown <opticalfalicy@gmail.com>
2018-09-17 13:45:38 -0500

    Modal solution

--- frontend/src/components/portfolio/index.js
+++ frontend/src/components/portfolio/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import Modal from 'react-modal';
 
 import { getProjects } from '../../actions';
-import { connect } from 'react-redux';
-import { Modal } from 'reactstrap';
-
 import './portfolio.css';
 
 const customStyles = {
@@ -14,30 +13,40 @@ const customStyles = {
       right                 : 'auto',
       bottom                : 'auto',
       marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)',
-      width                 : '90%',
-      height                : '90%'
+      transform             : 'translate(-50%, -50%)'
     }
   };
-  
-
 
 class Portfolio extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            // projects: []
             modal: false
         };
         this.props.getProjects();
-        this.toggle = this.toggle.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
         // this.state={
         //     project_name: "test",
         //     project_thumb: URL,
         // }
     }
 
+    openModal() {
+        this.setState({modalIsOpen: true});
+      }
+
+      afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+      }
+    
+      closeModal() {
+        this.setState({modalIsOpen: false});
+      }
+    
 
     toggle() {
         this.setState({
@@ -50,38 +59,44 @@ class Portfolio extends Component{
         // const data = this.props.data;
         return(
             <div className="portfolio-page">     
-            <div onClick={this.openModal} className="project-card-outer">
-            <Modal
-                isOpen={this.state.modalIsOpen}
-                onAfterOpen={this.afterOpenModal}
-                onRequestClose={this.closeModal}
-                style={customStyles}
-                // className = 'portfolio-modal'
-                contentLabel="Example Modal"
-            >
+                <div className="project-card-outer">
+                {/* <button onClick={this.openModal}>Open Modal</button> */}
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
 
-                      <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-                        <button onClick={this.closeModal}>close</button>
-                         <div>I am a modal</div>
-            </Modal>
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
 
-            {this.props.projects.map(projItem => {
-                return (
-                <div className="project-card">
-                
-                    <div className="project-name">
-                        {projItem.title} 
-                    </div>
-                    <div className="project-thumb">
-                        {/* {this.state.project_thumb} */}
-                        <img src={projItem.image}/>
-                        {/* <img src='https://photos.google.com/u/1/photo/AF1QipOWPVvBb0b7gIbuD4_xhgvBQ63rCWt4OZ1ibQuU'/> */}
-                    </div>
-        
+
+
+                    {this.props.projects.map(projItem => { 
+                        return ( 
+                            <div className="project-card"> 
+                                <div className="project-name"> <button onClick={this.openModal}>{projItem.title}</button>
+                                      
+                                </div> 
+                                <div className="project-thumb"> 
+                                    {this.state.project_thumb} 
+                                    <img src={projItem.image}/> 
+                                    {/* <img src='https://photos.google.com/u/1/photo/AF1QipOWPVvBb0b7gIbuD4_xhgvBQ63rCWt4OZ1ibQuU'/> */}
+                                 </div> 
+                             </div>); 
+                     })}
                 </div>
-            );
-            })}
-            </div>
             </div>
         )
     }
