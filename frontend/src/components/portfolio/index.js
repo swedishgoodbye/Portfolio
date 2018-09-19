08f6ee11e4ac7090aad16d16e428a05f667b5f2d
Unknown <opticalfalicy@gmail.com>
2018-09-19 15:44:44 -0500

    Three placeholder portfolio panels finished

--- frontend/src/components/portfolio/index.js
+++ frontend/src/components/portfolio/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import { getProjects } from '../../actions';
 import './portfolio.css';
 
 
@@ -22,8 +23,23 @@ class Portfolio extends Component{
 
         return(
             <div className="portfolio-page">     
-     
 
+                    <h1 className="portfolio-title">Portfolio</h1>
+
+                    <div className="portfolio-panels">
+
+                    <div className="portfolio-panel random-panel">
+
+                    </div>
+
+                    <div className="portfolio-panel port-panel">
+
+                    </div>
+                    
+                    <div className="portfolio-panel git-panel">
+
+                    </div>
+                </div>
 
             </div>
         )
