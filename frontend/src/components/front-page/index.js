4ff489c72e59c3ecd7e5974d72fd1790d753fdd1
Unknown <opticalfalicy@gmail.com>
2018-11-13 15:18:16 -0600

    Nav to landing, consolidated landing CSS to index

--- frontend/src/components/front-page/index.js
+++ frontend/src/components/front-page/index.js
@@ -10,7 +10,13 @@ class Front extends Component{
                 <div className = 'front-text'>
                     Welcome
                 </div>
-            
+                <div className='temp-nav'>
+                    <button  className='nav-button'>Start</button>
+                    <button className='nav-button'>Portfolio</button>
+                    <button className='nav-button' >About</button>
+                    {/* <button onClick={() => scrollToComponent(this.Music, { offset: 0, align: 'top', duration: 800})} className='nav-button'>Music</button> */}
+                    <button className='nav-button'>Contact</button>
+                </div>
             </div>
         )
     }
