c1b103dff6113c64d2b76427b57945912ca4b76d
Unknown <opticalfalicy@gmail.com>
2018-11-13 16:43:04 -0600

    Landing page mobile first, color scheme, nav layout

--- frontend/src/components/front-page/index.js
+++ frontend/src/components/front-page/index.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 
-import './front.css';
-
 class Front extends Component{
 
     render(){
         return( 
-            <div className = 'front-page main-item'>
+            <div className = 'front-page'>
                 <div className = 'front-text'>
                     Welcome
                 </div>
-                <div className='temp-nav'>
-                    <button  className='nav-button'>Start</button>
-                    <button className='nav-button'>Portfolio</button>
-                    <button className='nav-button' >About</button>
-                    {/* <button onClick={() => scrollToComponent(this.Music, { offset: 0, align: 'top', duration: 800})} className='nav-button'>Music</button> */}
-                    <button className='nav-button'>Contact</button>
+                <div className='nav'>
+                    <a className='nav-item'>Portfolio</a>
+                    <a className='nav-item' >About</a>
+                    {/* <a onClick={() => scrollToComponent(this.Music, { offset: 0, align: 'top', duration: 800})} className='nav-item'>Music</a> */}
+                    <a className='nav-item'>Contact</a>
                 </div>
             </div>
         )
