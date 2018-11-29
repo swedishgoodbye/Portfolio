6d91de2a533f8a96d3aa6558c11ba14581436351
swedishgoodbye <opticalfalicy@gmail.com>
2018-11-29 16:00:52 -0600

    Update index.js

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -20,7 +20,9 @@ class About extends Component{
                         <div className='about-content'>
 
                             <img src={'https://lh3.googleusercontent.com/VbfEac-GSjJLSMghD4cHF-G4K6HlzIcIUYc_EQL3KKh8OOpV1Ew9KDXm2aN1dl7sjrq7tzv0M89qz6_vOGDBw68Boaq5hvFX0v4rg29IpCuG1223VEYlPX91D9JbF-ikVPG5T2mDQjDc-M0MEEkZCzMa5f9qcvlkUwKzteNFefkrY9tP6AolnwqqBbnLvJsbX5mUPCk19tvZB5ts741hxLsD4lMLr5dh_ZwPtO4_helQOIlMEB8l2h-qR27PIpDIKUyj0vv0PsKLgjeGXaonIgPwrotqq-YDK1iE_apAeH3-J3XKAMqMGzKTIObonCqHXb-d9d6WGJGii4BF9JgfX6OdvzQAXgLnQS3LEXeT4mpEtWo5ZDopwYMwWCAVHRCGW9sfLv50KbxL-6hZHxwRYlibNUb3YLxIGkq25kDMIdkdTInp4_cwmbncR1HO195l3cw2irenb5ZeBossqci_O76eQyR6FzReMmPWgd-gKSTCQ3xvQ4BKKPCj3CUrzX7wI1HBqo5iFSf-7Mq6TbtC88urptnIsraK5ZTcHZu-OveOZLPDGFtGgsO7EVl9SFPmnM_w2hHHTCyd7qvuMACG7I0Yybhp8lntQ3r27encm9LseN0HGIGzL694zX1N8GCbGQ0Zc_OaWzLjB-9U11o3rGmm=w361-h362-no'} 
-                            className='about-img' />
+                            className='about-img'
+                            alt='Picture of me'
+                            />
 
                             <h1 className='about-name'>
                                 <a className='about-name about-link' href='http://opticalfalicy.com'>
